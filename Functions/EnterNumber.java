8fff904d79af9d42e2352cdd416dca6e83d68412
Aditya chandel <adityachandel371@gmail.com>
2025-08-12 13:17:41 +0530

    Added improvements

--- Functions/EnterNumber.java
+++ Functions/EnterNumber.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class EnterNumber {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int positive = 0, negative = 0, zero = 0, i = 0;
-        do {
+        System.err.print("Enter 1 to start and 0 to stop : ");
+        int input = sc.nextInt();
+
+        int positive = 0, negative = 0, zero = 0;
+
+        while (input == 1) {
             System.out.print("Enter the number : ");
             int n = sc.nextInt();
 
@@ -17,8 +21,10 @@ public class EnterNumber {
             } else {
                 zero++;
             }
-            i++;
-        } while (i != 10);
+
+            System.err.print("Enter 1 to start and 0 to stop : ");
+            input = sc.nextInt();
+        }
 
         System.out.println("\nPositive numbers entered : " + positive);
         System.out.println("Negative numbers entered : " + negative);
